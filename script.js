8b00f05108c94a8360516172aa45f41f10f06a0d
vitinhoegabrizinipedrini <victor.andrade.silva24@escola.pr.gov.br>
2025-09-30 07:38:03 -0300

    aula3009

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const avanca = document.querySelectorAll('.btn-proximo');
 avanca.forEach(button => {
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' +
-    }
-    )
-}
+        const proximoPasso = 'passo-' + this. getAttribut ('data-proximo');
 
-)
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    } )
+})
